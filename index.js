39a405d089aa67f510e60d1ed2dfd43f35f0fca0
azlen <azlen@livingcode.org>
2016-05-11 19:34:39 -0700

    ipad fix

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ var controller = Leap.loop({
     //direction.push(direction[0])
     //direction.shift()
 
-    hands[hand_index].querySelector('.palm').setAttribute('position', position.map(x=>x/100).join(' '))
+    hands[hand_index].querySelector('.palm').setAttribute('position', position.map(function(x){return x/100}).join(' '))
 
     hand.fingers.forEach(function(finger, finger_index) {
-      hands[hand_index].querySelector('.fingers').children[finger_index].setAttribute('position', finger.tipPosition.map(x=>x/100).join(' '));
+      hands[hand_index].querySelector('.fingers').children[finger_index].setAttribute('position', finger.tipPosition.map(function(x){return x/100}).join(' '));
     })
     //hands[i].setAttribute('rotation', direction.map(x=>x*180).join(' '))
     //document.querySelector('#blob').style.transform = 'translate('+direction[0]*100+'px, '+direction[1]*100+'px)'
