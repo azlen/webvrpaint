ce88c5ba4510f332ff1793f14e3fedf6855fafb7
azlen <azlen@livingcode.org>
2016-05-12 18:27:15 -0700

    changing the IP

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var hands = [document.querySelector('#hand'), document.querySelector('#hand2')]
 
+function mapPosition(position) {
+  return position.map(function(x){return x/100});
+}
+
+var path = '';
+
 var controller = Leap.loop({
-  host: '10.32.111.76',
-  enableGestures: true,
+  host: '192.168.1.26',
+  //enableGestures: true,
   //optimizeHMD: true
 }, function(frame) {
   frame.hands.forEach(function(hand, hand_index) {
@@ -17,11 +23,16 @@ var controller = Leap.loop({
     //direction.push(direction[0])
     //direction.shift()
 
-    hands[hand_index].querySelector('.palm').setAttribute('position', position.map(function(x){return x/100}).join(' '))
+    hands[hand_index].querySelector('.palm').setAttribute('position', mapPosition(position).join(' '))
 
     hand.fingers.forEach(function(finger, finger_index) {
-      hands[hand_index].querySelector('.fingers').children[finger_index].setAttribute('position', finger.tipPosition.map(function(x){return x/100}).join(' '));
+      //hands[hand_index].querySelector('.fingers').children[finger_index].setAttribute('position', mapPosition(finger.tipPosition).join(' '));
+      finger.bones.forEach(function(bone, bone_index) {
+        hands[hand_index].querySelector('.fingers').children[finger_index].querySelector('.bones').children[bone_index].setAttribute('line', 'path: '+mapPosition(bone.prevJoint).join(' ')+', '+mapPosition(bone.nextJoint).join(' '))
+      })
     })
+    path += (path.length == 0 ? '':', ') + mapPosition(hand.fingers[1].tipPosition).join(' ');
+    //document.querySelector('#draw').setAttribute('line', 'path: '+path)
     //hands[i].setAttribute('rotation', direction.map(x=>x*180).join(' '))
     //document.querySelector('#blob').style.transform = 'translate('+direction[0]*100+'px, '+direction[1]*100+'px)'
   })
